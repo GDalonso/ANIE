a47719319642c1e8c2d80aa7d686624fcadbceba
gui <guilherme.castro@fatec.sp.gov.br>
2018-10-21 02:54:44 -0300

    askjdklsad

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def index():
             user = session['user_logged'] if 'user_logged' in session.keys else None
             return render_template('not_blind/index.html', titulo="Anie", posts=posts, user=user)
 
-    return render_template('select_type.html', titulo="Anie", logged=)
+    return render_template('select_type.html', titulo="Anie")
 
 @app.route('/postagens')
 def postlist():
