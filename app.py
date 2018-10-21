4d5a622e4e1713dfaf7ccded9ed2440212512ca0
Guilherme Lima <guilherme.oliveira95@fatec.sp.gov.br>
2018-10-21 01:18:13 -0300

    index

--- app.py
+++ app.py
@@ -21,15 +21,15 @@ def index():
         session['type_user'] = request.args.get('type_user')
     else:
 
-        posts.append({'nomeImagem': 'img/about-img.jpg', 'textoImagem': 'gfg'})
+        posts.append({'nomeImagem': 'img/about-img.jpg', 'textoImagem': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'})
         if 'type_user' in session.keys() and session['type_user'] == 'not_blind':
             #bancolista = dbretrieve()
-            return render_template('not_blind/index.html', titulo="Anie")
+            return render_template('not_blind/index.html', titulo="Anie", posts=posts)
         elif 'type_user' in session.keys() and session['type_user'] == 'blind':
             #bancolista = dbretrieve()
-            return render_template('not_blind/index.html', titulo="Anie")
+            return render_template('not_blind/index.html', titulo="Anie", posts=posts)
 
-    return render_template('select_type.html', titulo="Anie", posts=posts)
+    return render_template('select_type.html', titulo="Anie")
 
 if __name__ == '__main__':
     app.run()
