57cda3ad1af15a0f127212ebe4c6d68c5f7b92dd
Guilherme Lima <guilherme.oliveira95@fatec.sp.gov.br>
2018-10-20 23:34:13 -0300

    vaiporra

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, session, request
 from flask_compress import Compress
 import os
+from Database import dbretrieve
 
 # Configura a aplicação, os diretorios de CSS, JS, Imagens e fontes
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
+app = Flask(__name__, template_folder='templates', static_folder='static')
 # Define uma chave para o HEROKU
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'WYZ')
 
@@ -16,10 +17,10 @@ def index():
     if request.args.get('type_user') and not 'type_user' in session.keys():
         session['type_user'] = request.args.get('type_user')
     if 'type_user' in session.keys() and session['type_user'] == 'not_blind':
-        bancolista = dbretrieve()
+        #bancolista = dbretrieve()
         return render_template('not_blind/index.html', titulo="Anie")
-    elif 'type_user' in session.keys() and session['type_user'] == 'not_blind':
-        bancolista = dbretrieve()
+    elif 'type_user' in session.keys() and session['type_user'] == 'blind':
+        #bancolista = dbretrieve()
         return render_template('not_blind/index.html', titulo="Anie")
 
     return render_template('select_type.html', titulo="Anie")
